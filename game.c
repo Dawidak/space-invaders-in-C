35493756fe68f00fb79516b99ffed0558f31bf36
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2022-12-21 21:23:43 +0100

    update

--- game.c
+++ game.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define windoWidth 21
 #define windowHeight 15
 #define hudHeight 3
 
 
 void box(int ekran[][windoWidth]){
-    for (int i = 0; i < windowHeight; i++){for (int j = 0; j < windoWidth; j++){ekran[i][j] = 219;}} 
+    for (int i = 0; i < windowHeight; i++){for (int j = 0; j < windoWidth; j++){ekran[i][j] = 254;}} 
     for (int i = 1; i < windowHeight-1; i++){for (int j = 1; j < windoWidth-1; j++){ekran[i][j] = 45;}}
 }
 
-void line(int znak, int dlugosc,const char how[],const char color[]){
+void line(int znak, int dlugosc,const char how[]){
     if(how=="style"){
         printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-    }else{for (int i = 0; i < dlugosc; i++){printf(color);printf(how,znak);}}
+    }else{for (int i = 1; i < dlugosc; i++){printf(how,znak);}}
+    printf(how,219);
     printf("\n");
 }
 
 void display(int ekran[][windoWidth],int hud[][windoWidth],const char how[] ){
     //box(ekran);
-    line(219,windoWidth+2,how,"\033[1;31m");
+    printf(how,219);
+    line(254,windoWidth+1,how);
     
     for (int i = 0; i < windowHeight; i++){
         printf(how,219);
@@ -31,8 +34,8 @@ void display(int ekran[][windoWidth],int hud[][windoWidth],const char how[] ){
         printf(how,219);
         printf("\n");
     }
-
-    line(219,windoWidth+2,how,"");
+printf(how,219);
+    line(254,windoWidth+1,how);
 
     for (int i = 0; i < hudHeight; i++){
         printf(how,219);
@@ -44,8 +47,8 @@ void display(int ekran[][windoWidth],int hud[][windoWidth],const char how[] ){
         printf(how,219);
         printf("\n");
     }
-    
-    line(219,windoWidth+2,how,"");
+    printf(how,219);
+    line(254,windoWidth+1,how);
 }
 
 int main (int argc, char *argv[]) {
