eb9b8be105eb8b35fa56ce2e135ecae07ab35e50
Dawidak <57863506+Dawidak@users.noreply.github.com>
2022-12-21 19:01:03 +0000

    tencza!

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("\033[0;30m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[1;31m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0;32m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0;33m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0;34m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0;35m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0;36m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0;37m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    printf("\033[0m");
+    printf("%c%c%c%c%c%c%c \n", 219,219,219,219,219,219,219);
+    return 0;
+}
